9dd062d8cfcb108bcd5ff54ffb75381c94ecc503
Matteo <45847997+sanriodev@users.noreply.github.com>
2025-07-31 15:39:36 +0200

    Update goji.rb

--- Formula/goji.rb
+++ Formula/goji.rb
@@ -3,7 +3,7 @@ class Goji < Formula
   homepage "https://github.com/sanriodev/goji"
   url "https://github.com/sanriodev/goji/archive/refs/tags/v1.4.3.tar.gz"
   version "1.4.3"
-  sha256 "9543a521a9f59fd8fba8b33681ef905122826740c82a78acdedac2dce9b15d3c"
+  sha256 "001f77b5fe67cc9c1243977f020b7f6162be0d5cae46b18564048f1f5bafe331"
   license "MIT"
 
   depends_on "go" => :build
